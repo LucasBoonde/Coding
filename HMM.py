4fb255b711675dd7c539032d2462bc69dafd36d2
lucasbonde <lucas.nesgaard.bonde@gmail.com>
2024-03-26 13:33:32 +0100

    Working on CNN with pretrained models

--- HMM.py
+++ HMM.py
@@ -7,4 +7,7 @@ transition_distributions = tfd.Categorical(probs=[0.7, 0.3])
 
 observation_distributions = tfd.Normal(loc=[0.,15.],scale=[5.,10.])#loc er din mean, og scale er din std.
 
-model = tfd.HiddenMarkModel(initial_distribution=initial_distributions, transition_distribution = transition_distributions, observation_distribution = observation_distributions, num_steps=7)
+model = tfd.HiddenMarkovModel(initial_distribution=initial_distributions, transition_distribution = transition_distributions, observation_distribution = observation_distributions, num_steps=7)
+
+mean = model.mean()
+print(mean.numpy())
